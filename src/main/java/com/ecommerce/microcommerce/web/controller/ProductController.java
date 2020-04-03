9733cd611eab723205d5aad774f89bda15ede2e4
mustapha <m.touil.mt@gmail.com>
2020-04-03 03:48:43 +0200

    modification de la méthode Post afin de récupérer l id du nouveau produit crée via Postman pour retourner 201create test ok

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -4,8 +4,11 @@ import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.dao.ProductDaoImpl;
 import com.ecommerce.microcommerce.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,23 @@ public class ProductController {
         return productDao.findById(id);
     }
 
+    //ajouter un produit
     @PostMapping(value = "/Produits")
-    public void ajouterProduit(@RequestBody Product product){
-        productDao.save(product);
+    public ResponseEntity<Void> ajouterProduit(@RequestBody Product product) {
+
+        Product productAdded =  productDao.save(product);
+
+        if (productAdded == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productAdded.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
+
+
 }
